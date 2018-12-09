990f709dccdee70dbc84ca3d72d5d87176826d26
nicGardner <nicgardner@rogers.com>
2018-12-09 11:13:30 -0500

    fully implemented sort by functionality

--- src/controllers/GUIController.java
+++ src/controllers/GUIController.java
@@ -3,15 +3,16 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import models.Inventory;
 import models.Product;
 import models.PsudeauDB;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class GUIController implements Initializable {
@@ -22,9 +23,26 @@ public class GUIController implements Initializable {
 
     @FXML private ChoiceBox<String> choiceBox;
 
+    @FXML private Button resetSortButton;
+    @FXML private Button sellButton;
+
+    final ToggleGroup sortGroup = new ToggleGroup();
+    @FXML private RadioButton highLow = new RadioButton();
+    @FXML private RadioButton lowHigh;
+    @FXML private RadioButton aZ;
+    @FXML private RadioButton zA;
+
+    private int optionPressed = 0;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        // put all the radio buttons in the same toggle group
+        highLow.setToggleGroup(sortGroup);
+        lowHigh.setToggleGroup(sortGroup);
+        aZ.setToggleGroup(sortGroup);
+        zA.setToggleGroup(sortGroup);
 
         // load all products into the table
         nameColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("name"));
@@ -39,7 +57,15 @@ public class GUIController implements Initializable {
 
     public void popTable()
     {
-        table.getItems().addAll(Inventory.getAllProducts());
+        ArrayList<Product> list = new ArrayList<>();
+        list.addAll(Inventory.getAllProducts());
+        // if one of the buttons was pressed
+        if (optionPressed >= 1 && optionPressed <= 4)
+        {
+            Collections.sort(list);
+        }
+        System.out.println(list);
+        table.getItems().addAll(list);
     }
 
     public void reloadTable()
@@ -52,5 +78,39 @@ public class GUIController implements Initializable {
     {
         table.getSelectionModel().getSelectedItem().sellStock();
         reloadTable();
+    }
+
+    public void highLowPressed(ActionEvent actionEvent) {
+        optionPressed = 1;
+        Product.setSortBy(1);
+        reloadTable();
+    }
+
+    public void lowHighPressed(ActionEvent actionEvent) {
+        optionPressed = 2;
+        Product.setSortBy(2);
+        reloadTable();
+    }
+
+    public void aZPressed(ActionEvent actionEvent) {
+        optionPressed = 3;
+        Product.setSortBy(3);
+        reloadTable();
+    }
+
+    public void zAPressed(ActionEvent actionEvent) {
+        optionPressed = 4;
+        Product.setSortBy(4);
+        reloadTable();
+    }
+
+    public void resetSortButtonPressed(ActionEvent actionEvent) {
+        optionPressed = 0;
+        Product.setSortBy(0);
+        reloadTable();
+        highLow.setSelected(false);
+        lowHigh.setSelected(false);
+        aZ.setSelected(false);
+        zA.setSelected(false);
     }
 }
--- src/models/Product.java
+++ src/models/Product.java
@@ -2,12 +2,15 @@ package models;
 
 import javafx.scene.image.Image;
 
-public class Product {
+public class Product implements Comparable<Product>{
 
     private String name, desc;
     private Image img;
     private double price;
     private int stock;
+
+    // specifies which method of sorting is being used.  1 = price high-low, 2 = price low-high, 3 = name a-z, 4 = name z-a
+    private static int sortBy = 1;
 
     public Product(String name, String desc, Image img, double price, int stock)
     {
@@ -72,6 +75,65 @@ public class Product {
         {
             System.out.println("no stock to sell");
         }
+
+    }
+
+    public static void setSortBy(int i)
+    {
+        sortBy = i;
+    }
+
+    @Override
+    public int compareTo(Product p)
+    {
+        System.out.println(sortBy);
+        if(sortBy == 1)
+        {
+            // sorts price high-low
+            if (this.getPrice() > p.getPrice())
+            {
+                return -1;
+            }
+            else if (this.getPrice() < p.getPrice())
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if (sortBy == 2)
+        {
+            // sorts price low-high
+            if (this.getPrice() > p.getPrice())
+            {
+                return 1;
+            }
+            else if (this.getPrice() < p.getPrice())
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if (sortBy == 3)
+        {
+            // sorts alphabetically a-z
+            return this.getName().compareToIgnoreCase(p.getName());
+        }
+        else if (sortBy == 4)
+        {
+            // sorts alphabetically z-a
+            return (this.getName().compareToIgnoreCase(p.getName()))*-1;
+        }
+
+        // default to sorting alphabetically a-z
+        //return this.getName().compareToIgnoreCase(p.getName());
+        // default to not sorting
+        return 0;
 
     }
 
